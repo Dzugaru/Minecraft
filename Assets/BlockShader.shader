5d81ffa2a001ace67cb716e7071eeeded84de32a
Dzugaru <dzugaru@gmail.com>
2016-10-20 19:42:32 +0300

    ---

---
+++ Assets/BlockShader.shader
@@ -0,0 +1,64 @@
+﻿Shader "Custom/BlockShader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+		_AtlasSize ("AtlasSize", Int) = 1
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+
+		struct Input {
+			float4 color : COLOR;
+			float3 worldPos;
+			float3 worldNormal;
+		};
+
+		half _Glossiness;
+		half _Metallic;
+		fixed4 _Color;
+		uint _AtlasSize;		
+		
+		static const float blockUvStep = 1.0 / (int)(_AtlasSize);			
+
+		void surf (Input IN, inout SurfaceOutputStandard o) 
+		{			
+			uint ti = (IN.color.r * 255.0) - 1;
+
+			/*if (ti == 2)
+				o.Albedo = float3(0, 1, 0);
+			else if(ti == 1)
+				o.Albedo = float3(1, 0, 0);
+			else
+				o.Albedo = float3(1, 1, 1);*/
+
+			uint i = ti / _AtlasSize;
+			uint j = ti - i * _AtlasSize;
+
+			float2 tileUvFull = float2(dot(IN.worldNormal.zxy, IN.worldPos),
+				dot(IN.worldNormal.yzx, IN.worldPos));
+			float2 tileUv = frac(tileUvFull);			
+			float2 uv = (float2(i, j) + tileUv) * blockUvStep;
+
+			//We need manually computed dx and dy because texture sampler selects mip level according to d(uv)/dx etc. and
+			//using frac(tileUv) leads to incorrect mip level selection at the seams
+			o.Albedo = tex2D(_MainTex, uv, ddx(tileUvFull * blockUvStep), ddy(tileUvFull * blockUvStep)).rgb;
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			o.Alpha = 1;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
--- Assets/Code/Mesher.cs
+++ Assets/Code/Mesher.cs
@@ -6,13 +6,20 @@ using System.Linq;
 public class Mesher : MonoBehaviour
 {
     List<Vector3> verts;
-    List<int> idxs;
+    List<Color32> colors;
+    List<int> idxs;    
+       
+    public string blocksFolder;
+    public int blocksSize;
+    public List<string> blocks;
 
-    public float scale = 0.1f;
+    public float noiseScale;
+    
 
 	void Start()
     {
         verts = new List<Vector3>();
+        colors = new List<Color32>();
         idxs = new List<int>();
 
         //int[,,] test = new int[,,]
@@ -46,12 +53,24 @@ public class Mesher : MonoBehaviour
         //});
 
         RangeI r = new RangeI(0, 20);
-        GenMesh(p => DebugSphereGen(new PointI(10, 10, 10), 8, p), new RangeI[] { r, r, r });
+        GenMesh(p => DebugSphereGen(new PointI(10, 10, 10), 8, p), 2, new RangeI[] { r, r, r });
 
         Mesh mesh = GetComponent<MeshFilter>().mesh;
         mesh.vertices = verts.ToArray();
+        mesh.colors32 = colors.ToArray();
         mesh.triangles = idxs.ToArray();
-        mesh.RecalculateNormals();        
+                
+        mesh.RecalculateNormals();
+
+        int w;
+        Texture2D atlas = ConstructAtlas(blocksFolder, blocks, blocksSize, out w);
+        atlas.filterMode = FilterMode.Point;        
+        System.IO.File.WriteAllBytes(Application.persistentDataPath + "/testAtlas.png", atlas.EncodeToPNG());
+
+        Material mat = GetComponent<MeshRenderer>().sharedMaterial;
+        mat.SetInt("_AtlasSize", w);
+        mat.SetInt("_BlockSize", blocksSize);
+        mat.SetTexture("_MainTex", atlas);       
 	}	
 	
 	void Update()
@@ -62,7 +81,7 @@ public class Mesher : MonoBehaviour
     int DebugCubeGen(PointI p)
     {
         for (int i = 0; i < 3; i++)        
-            if (p[i] < 1 || p[i] > 3) return 0;        
+            if (p[i] < 1 || p[i] > 3) return 0;
 
         return 1;
     }    
@@ -73,101 +92,107 @@ public class Mesher : MonoBehaviour
                                 (p.Y - c.Y) * (p.Y - c.Y) +
                                 (p.Z - c.Z) * (p.Z - c.Z));
 
+        if (dist > r) return 0;
         
-        return dist <= r ? 1 : 0;
+        float nv = (float)SimplexNoise.noise(p.X / noiseScale, p.Y / noiseScale, p.Z / noiseScale);
+        return nv < 0 ? 1 : 2;                
+        //return p.X < c.X ? 1 : 2;
     }
 
-    void GenMesh(Func<PointI, int> gen, RangeI[] ranges)
-    {        
-        for(int d = 0; d < 3; d++)
+    void GenMesh(Func<PointI, int> gen, int maxTerrIdx, RangeI[] ranges)
+    {
+        for (int ti = 1; ti <= maxTerrIdx; ti++)
         {
-            int id, jd;
-            switch (d)
+            for (int d = 0; d < 3; d++)
             {
-                case 0: id = 1; jd = 2; break;
-                case 1: id = 2; jd = 0; break;
-                case 2: id = 0; jd = 1; break;
-                default: throw new InvalidProgramException();
-            }
-
-            int ni = ranges[id].Max - ranges[id].Min;
-            int nj = ranges[jd].Max - ranges[jd].Min;
-            
-            for (int dir = 0; dir < 2; dir++)
-            {               
-                for (int k = ranges[d].Min; k < ranges[d].Max - 1; k++)
+                int id, jd;
+                switch (d)
                 {
-                    //Filling slice
-                    int[,] slice = new int[ni, nj];
-                    int n = 0;
-                    for (int i = 0; i < ni; i++)
+                    case 0: id = 1; jd = 2; break;
+                    case 1: id = 2; jd = 0; break;
+                    case 2: id = 0; jd = 1; break;
+                    default: throw new InvalidProgramException();
+                }
+
+                int ni = ranges[id].Max - ranges[id].Min;
+                int nj = ranges[jd].Max - ranges[jd].Min;
+
+                for (int dir = 0; dir < 2; dir++)
+                {
+                    for (int k = ranges[d].Min; k < ranges[d].Max - 1; k++)
                     {
-                        for (int j = 0; j < nj; j++)
+                        //Filling slice
+                        int[,] slice = new int[ni, nj];
+                        int n = 0;
+                        for (int i = 0; i < ni; i++)
                         {
-                            PointI p0 = new PointI(3);
-                            p0[d] = k;
-                            p0[id] = i + ranges[id].Min;
-                            p0[jd] = j + ranges[jd].Min;
-                            PointI p1 = p0.Clone();
-                            p1[d]++;
-
-                            int v0 = gen(p0);
-                            int v1 = gen(p1);
-
-                            if(dir == 0 && v0 > 0 && v1 == 0 ||
-                               dir == 1 && v0 == 0 && v1 > 0)
+                            for (int j = 0; j < nj; j++)
                             {
-                                slice[i, j] = 1;
-                                n++;
-                            }                            
-                        }
-                    }
+                                PointI p0 = new PointI(3);
+                                p0[d] = k;
+                                p0[id] = i + ranges[id].Min;
+                                p0[jd] = j + ranges[jd].Min;
+                                PointI p1 = p0.Clone();
+                                p1[d]++;
 
-                    //Draw quads greedy
-                    //int debugI = 0;
-                    while(n > 0)
-                    {
-                        int l = 0, t = 0;
+                                int v0 = gen(p0);
+                                int v1 = gen(p1);
 
-                        //Find left top
-                        for (int i = 0; i < ni; i++)                        
-                            for(int j = 0; j < nj; j++)                            
-                                if(slice[i,j] == 1)
+                                if (dir == 0 && v0 == ti && v1 == 0 ||
+                                   dir == 1 && v0 == 0 && v1 == ti)
                                 {
-                                    t = i;
-                                    l = j;
-                                    goto Exit1;
+                                    slice[i, j] = 1;
+                                    n++;
+                                }
+                            }
+                        }
+
+                        //Draw quads greedy
+                        //int debugI = 0;
+                        while (n > 0)
+                        {
+                            int l = 0, t = 0;
+
+                            //Find left top
+                            for (int i = 0; i < ni; i++)
+                                for (int j = 0; j < nj; j++)
+                                    if (slice[i, j] == 1)
+                                    {
+                                        t = i;
+                                        l = j;
+                                        goto Exit1;
+                                    }
+
+                            Exit1:
+                            //Find width
+                            int w = 1;
+                            for (int j = l + 1; j < nj; j++, w++)
+                                if (slice[t, j] == 0) break;
+
+                            //Find height
+                            int h = 1;
+                            for (int i = t + 1; i < ni; i++, h++)
+                                for (int j = l; j < l + w; j++)
+                                    if (slice[i, j] == 0) goto Exit2;
+
+                            Exit2:
+                            //Erase slice
+                            for (int i = t; i < t + h; i++)
+                                for (int j = l; j < l + w; j++)
+                                {
+                                    slice[i, j] = 0;
+                                    n--;
                                 }
 
-                        Exit1:
-                        //Find width
-                        int w = 1;
-                        for (int j = l + 1; j < nj; j++, w++)                        
-                            if (slice[t, j] == 0) break;
+                            DrawQuad(d, dir, k, ti, new RangeI(t, t + h), new RangeI(l, l + w));
 
-                        //Find height
-                        int h = 1;
-                        for (int i = t + 1; i < ni; i++, h++)
-                            for (int j = l; j < l + w; j++)
-                                if (slice[i, j] == 0) goto Exit2;
-
-                        Exit2:
-                        //Erase slice
-                        for (int i = t; i < t + h; i++)
-                            for (int j = l; j < l + w; j++)
-                            {
-                                slice[i, j] = 0;
-                                n--;
-                            }
-                        
-                        DrawQuad(d, dir, k, new RangeI(t, t + h), new RangeI(l, l + w));
-
-                        //debugI++;
-                        //if (debugI > 100) break;
+                            //debugI++;
+                            //if (debugI > 100) break;
+                        }
                     }
                 }
             }
-        }
+        }        
     }
 
     Vector3 FillVector3(int d, float v0, float v1, float v2)
@@ -181,7 +206,7 @@ public class Mesher : MonoBehaviour
         }
     }
 
-    private void DrawQuad(int d, int dir, int k, RangeI rangeI1, RangeI rangeI2)
+    private void DrawQuad(int d, int dir, int k, int ti, RangeI rangeI1, RangeI rangeI2)
     {
         //Debug.Log("(" + d + "," + dir + "," + k + ") " + rangeI1.Min + " " + rangeI2.Min + " " + rangeI1.Max + " " + rangeI2.Max);
 
@@ -189,14 +214,48 @@ public class Mesher : MonoBehaviour
 
         int i = verts.Count;
 
-        verts.Add(FillVector3(d, k, rangeI1.Min, rangeI2.Min) * scale);
-        verts.Add(FillVector3(d, k, rangeI1.Max, rangeI2.Min) * scale);
-        verts.Add(FillVector3(d, k, rangeI1.Max, rangeI2.Max) * scale);
-        verts.Add(FillVector3(d, k, rangeI1.Min, rangeI2.Max) * scale);
+        verts.Add(FillVector3(d, k, rangeI1.Min, rangeI2.Min));
+        verts.Add(FillVector3(d, k, rangeI1.Max, rangeI2.Min));
+        verts.Add(FillVector3(d, k, rangeI1.Max, rangeI2.Max));
+        verts.Add(FillVector3(d, k, rangeI1.Min, rangeI2.Max));
+
+        Color32 color = new Color32((byte)ti, 0, 0, 0);
+        for (int j = 0; j < 4; j++) colors.Add(color);
 
         if (dir == 0)
             idxs.AddRange(new[] { i,i+1,i+3,i+3,i+1,i+2 });
         else
             idxs.AddRange(new[] { i,i+3,i+1,i+3,i+2,i+1 });
+    }
+
+    Texture2D ConstructAtlas(string folder, List<string> names, int size, out int w)
+    {
+        w = 1;
+        while (w * w < blocks.Count) w *= 2;
+
+        Texture2D atlas = new Texture2D(w * size, w * size, TextureFormat.ARGB32, true);
+        int i = 0, j = 0;      
+        foreach(string name in names)
+        {
+            string fullName = folder + "/" + name;            
+            Texture2D tex = Resources.Load<Texture2D>(fullName);           
+
+            int msize = size;
+            for (int m = 0; m < tex.mipmapCount; m++)           
+            {
+                atlas.SetPixels32(i * msize, j * msize, msize, msize, tex.GetPixels32(m), m);               
+                msize /= 2;
+            }
+
+            j++;
+            if(j >= w)
+            {
+                j = 0;
+                i++;
+            }
+        }
+
+        atlas.Apply();
+        return atlas;
     }
 }
